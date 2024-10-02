1d8d0ed6500a76b937df4528dce25e8b303bbafe
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2024-10-01 18:59:20 -0600

    updates

--- change.js
+++
@@ -1,7 +0,0 @@
-const before = document.getElementById("before");
-const after = document.getElementById("after");
-const button = document.getElementById("button1");
-
-function change() {
-  before.remove();
-}
